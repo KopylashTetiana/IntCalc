4a4336fd04b71757ad84cf368619b5cd899c4349
KopylashTetiana <tresp9@gmail.com>
2022-08-25 18:31:11 +0300

    Rename file Program.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import Calc.Context;
 
 import java.io.IOException;
 
-public class Program { //entrance to the app
+public class Main { //entrance to the app
     public static void main(String[] args) throws IOException {
         System.out.println("Welcome to the Integer Calculator!");
         Context calc = new Context();
